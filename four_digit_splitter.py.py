0c22179c0d4d9ba4772c3ed7960cad929f4752ae
Anthony Behnam <anthony.p.behnam@outlook.com>
2024-10-07 02:40:44 -0400

    first commit

---
+++ four_digit_splitter.py.py
@@ -0,0 +1,12 @@
+num = int(input('Enter a four-digit integer: '))
+n1 = num % 10
+num = num // 10
+n2 = num % 10
+num = num // 10
+n3 = num % 10
+num = num // 10
+n4 = num % 10
+print(n1)
+print(n2)
+print(n3)
+print(n4)
